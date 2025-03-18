afa78f5fdecb0e1d369958e895bb5a83ae8978d2
TheCrazyHitomi <amandine-tomas@hotmail.fr>
2025-03-19 00:11:18 +0100

    create footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,35 @@
-const footer = () => {
+const Footer = () => {
+
     return (
-        <footer>
-        <p>Le pied de page</p>
-        </footer>
+        <footer className="d-flex bg-secondary text-black text-center p-2 p-lg-4 w-100 justify-content-evenly align-content-center font-monospace">
+        <div className="row align-items-center justify-content-center  w-auto">
+            <div className="col-5 col-lg-4 text-end w-auto">
+                <p className="responsive-text">12 Boulevard Robert Barrier <br /> 
+                73100 Aix-les-Bains<br/>
+                <br/>
+                quai-antique@contact.fr</p>
+            </div>
+            <div className="col-2 vr w-3 p-0 m-1 bg-danger opacity-100 rounded-1 "></div>
+            <div className="col-5 col-lg-4 ps-md-2  pe-md-2  text-start w-auto">
+                <p className="responsive-text">Ouvert <br />
+                du Mardi au Dimanche <br />
+                service de <br />
+                12h-14h & 19h30-21h30
+                </p>
+        </div>
+            </div>
+            <div className="d-none d-lg-block col-lg-4 text-end w-auto">
+            <nav className="nav flex-column  ">
+                <a className="responsive-text text-decoration-none text-black" href="/">Accueil</a>
+                <a className="responsive-text text-decoration-none text-black" href="/galerie">Galerie</a>
+                <a className="responsive-text text-decoration-none text-black" href="/">Notre Carte</a>
+                <a className="responsive-text text-decoration-none text-black" href="/">Réservation</a>
+                <a className="responsive-text text-decoration-none text-black" href="/">Mon Compte</a>
+                <a className="responsive-text text-decoration-none text-light" href="/">Connexion</a>
+            </nav>
+            </div>
+    </footer>
     )
     }
 
-export default footer
+export default Footer
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,24 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Header = () => {
     return (
-    <Navbar expand="lg" className="row justify-content-around bg-secondary" data-bs-theme="dark">
+    <Navbar expand="lg" className="row justify-content-around bg-dark" data-bs-theme="dark" >
       <Container>
         <Navbar.Brand href="/" className='ms-2 ms-md-5'>
             <img src="public/images/logo.png" alt="Quai Antique Logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className='me-2 me-md-5'/>
-        <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="ms-auto me-5">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className='z-3 position-relative me-2 me-md-5 p-2 border-0'/>
+        <Navbar.Collapse id="basic-navbar-nav" className='z-1 position-absolute top-0 end-0 bg-dark rounded-bottom-3 mw-30 me-0 mt-10 p-2'>
+          <Nav className="d-flex ms-auto me-5 align-items-end">
             <Nav.Link href="/">Accueil</Nav.Link>
             <Nav.Link href="/galerie">Galerie</Nav.Link>
             <Nav.Link href="/">Notre Carte</Nav.Link>
             <Nav.Link href="/">Réservation</Nav.Link>
             <Nav.Link href="/">Mon Compte</Nav.Link>
-            <Nav.Link className="text-info" href="/">Connexion</Nav.Link>
+            <Nav.Link className="text-light" href="/">Connexion</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
