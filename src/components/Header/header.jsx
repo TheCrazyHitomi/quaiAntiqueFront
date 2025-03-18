37eab4c47615c1a5237c647379a9eab5eec9d18e
TheCrazyHitomi <amandine-tomas@hotmail.fr>
2025-03-18 18:41:24 +0100

    create Header component

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 const Header = () => {
     return (
-        <header>
-        <i class="bi bi-apple"></i>
-        <h1>My App</h1>
-        </header>
-    );
-    };
+    <Navbar expand="lg" className="row justify-content-around bg-secondary" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="/" className='ms-2 ms-md-5'>
+            <img src="public/images/logo.png" alt="Quai Antique Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className='me-2 me-md-5'/>
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="ms-auto me-5">
+            <Nav.Link href="/">Accueil</Nav.Link>
+            <Nav.Link href="/galerie">Galerie</Nav.Link>
+            <Nav.Link href="/">Notre Carte</Nav.Link>
+            <Nav.Link href="/">Réservation</Nav.Link>
+            <Nav.Link href="/">Mon Compte</Nav.Link>
+            <Nav.Link className="text-info" href="/">Connexion</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default Header;
