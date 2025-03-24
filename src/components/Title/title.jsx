4243f74dea319aa30003b5cf7ab82416c52a93c2
TheCrazyHitomi <amandine-tomas@hotmail.fr>
2025-03-24 18:10:09 +0100

    fixing carousel CSS

--- src/components/Title/title.jsx
+++ src/components/Title/title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
     return (
             <div className="d-flex flex-column justify-content-center align-items-center w-100 h-100">
-                <h1 className='responsive-title'>bienvenue</h1>
+                <h1 className='responsive-title'>Bienvenue</h1>
                 <hr className="bg-primary"/>
                 <h1 className='responsive-title'>au Quai Antique</h1>
             </div>
